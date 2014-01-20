f48c3056aaae167c2623e28450e9b829f8c9e4e2
Marcello Barnaba <vjt@openssl.it>
2014-01-20 18:55:34 +0100

    Define engine name

--- lib/user_error/engine.rb
+++ lib/user_error/engine.rb
@@ -1,5 +1,6 @@
 module UserError
   class Engine < ::Rails::Engine
+    engine_name :user_error
 
     initializer "user_error.install_app" do |app|
       app.config.exceptions_app = ->(env) { ::UserError::ExceptionsController.action(:show).call(env) }
