5fa7651e0775d2dff513e1656a48de559737a214
Lleïr Borràs Metje <lleir@llegeix.me>
2014-01-21 11:53:16 +0100

    Add some tests with different formats

--- spec/features/error_pages_spec.rb
+++ spec/features/error_pages_spec.rb
@@ -11,6 +11,25 @@ feature "UserError handles the exeptions" do
     page.should have_content "Internal Server Error"
   end
 
+  scenario "JSON server error" do
+    visit '/tests.json'
+    json = JSON.parse(page.source)
+    expect(json['error']['name']).to eq('ActiveRecord::ActiveRecordError' )
+    expect(json['error']['status']).to eq(500 )
+  end
+
+  scenario "XML server error" do
+    visit '/tests.xml'
+    xml = Nokogiri.parse(page.source)
+    expect( xml.xpath('//error/name').inner_html).to eq('ActiveRecord::ActiveRecordError')
+    expect( xml.xpath('//error/status').inner_html).to eq('500')
+  end
+
+  scenario "Other server error" do
+    visit '/tests.csv'
+    expect(page.source).to be_blank
+  end
+
   scenario "bad request" do
     visit '/tests/1'
     page.should have_content "Forbidden"
