4464cea9538006cb13a4cd7b7cc4a27178f4d96e
Marcello Barnaba <vjt@openssl.it>
2015-07-14 10:20:46 +0200

    Don't change application settings

--- lib/user_error/engine.rb
+++ lib/user_error/engine.rb
@@ -1,4 +1,5 @@
 module UserError
+
   class Engine < ::Rails::Engine
     isolate_namespace UserError
 
@@ -7,13 +8,6 @@ module UserError
     initializer "user_error.install_app" do |app|
       app.config.exceptions_app = ->(env) { ::UserError::ExceptionsController.action(:show).call(env) }
     end
-
-    config.generators do |g|
-      g.test_framework      :rspec,        fixture: false
-      g.fixture_replacement :factory_girl,     dir: 'spec/factories'
-      g.assets false
-      g.helper false
-    end
-
   end
+
 end
